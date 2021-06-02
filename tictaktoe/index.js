5f16ae555f36d0c66b7122d6bbed49fd62c5c4c0
juuunobae <juuunobae@gmail.com>
2021-06-02 16:10:47 +0900

    tictactoe

---
+++ tictaktoe/index.js
@@ -0,0 +1,30 @@
+const $table = document.createElement("table");
+
+let turn = "O";
+const rows = [];
+for (let i = 0; i < 3; i++) {
+  const $tr = document.createElement("tr");
+  const cells = [];
+  for (let j = 0; j < 3; j++) {
+    const $td = document.createElement("td");
+    cells.push($td);
+    $td.addEventListener("click", (e) => {
+      if (e.target.textContent) return;
+      e.target.textContent = turn;
+      if (turn === "O") {
+        turn = "X";
+      } else if (turn === "X") {
+        turn = "O";
+      }
+    });
+    $tr.append($td);
+  }
+  rows.push(cells);
+  $table.append($tr);
+}
+document.body.append($table);
+
+const data = [];
+for (let i = 0; i < 3; i++) {
+  data.push([]);
+}
