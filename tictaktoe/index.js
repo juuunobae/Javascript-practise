df6baa8ff6a4bd8e3877afb38b8d65da67b5a226
juuunobae <juuunobae@gmail.com>
2021-06-04 16:18:37 +0900

    tictaktoe

--- tictaktoe/index.js
+++ tictaktoe/index.js
@@ -1,5 +1,15 @@
 const $table = document.createElement("table");
 
+const callback = dsfd(e) => {
+  if (e.target.textContent) return;
+  e.target.textContent = turn;
+  if (turn === "O") {
+    turn = "X";
+  } else if (turn === "X") {
+    turn = "O";
+  }
+};
+
 let turn = "O";
 const rows = [];
 for (let i = 0; i < 3; i++) {
@@ -8,15 +18,7 @@ for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
     const $td = document.createElement("td");
     cells.push($td);
-    $td.addEventListener("click", (e) => {
-      if (e.target.textContent) return;
-      e.target.textContent = turn;
-      if (turn === "O") {
-        turn = "X";
-      } else if (turn === "X") {
-        turn = "O";
-      }
-    });
+    $td.addEventListener("click", callback);
     $tr.append($td);
   }
   rows.push(cells);
